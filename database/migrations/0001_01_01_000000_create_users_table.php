fdb2c4f6550bee068cfd42cf0f9b1630f3b6e5e2
BlacOnchain <odubelatomiwa508@gmail.com>
2026-09-11 02:46:42 +0100

    Quick update: 09/11/2026 02:46:42

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,21 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-    $table->id();
-    $table->string('name');
-    $table->string('email')->unique();
-    $table->timestamp('email_verified_at')->nullable();
-    $table->string('password');
-
-    // Add these here:
-    $table->string('matric_number')->nullable();
-    $table->string('phone_number')->nullable();
-    $table->string('department')->nullable();
-    $table->string('level')->nullable();
-
-    $table->rememberToken();
-    $table->timestamps();
-});
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('matric_number')->nullable();
+            $table->string('department')->nullable();
+            $table->string('level')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
